2c0d45ec799be41c8eeca90b81f5f9fc4d3bb92c
billytrinh <quanghoa.trinh@gmail.com>
2022-12-12 11:52:11 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {StudentComponent} from "./student/student.component";
+import {ClassesComponent} from "./classes/classes.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,StudentComponent,ClassesComponent
   ],
   imports: [
     BrowserModule
