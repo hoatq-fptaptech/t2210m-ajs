39fe16b745776b3fd3a462ad76048dfd93585515
billytrinh <quanghoa.trinh@gmail.com>
2022-12-14 09:45:33 +0700

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {StudentComponent} from "./student/student.component";
 import {ClassesComponent} from "./classes/classes.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {CategoryComponent} from "./pages/category/category.component";
+import { ProductComponent } from './pages/product/product.component';
+import {RouterModule, Routes} from "@angular/router";
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
+//1. khai bao danh sach cac routing
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'category', component: CategoryComponent},
+  { path: 'product', component: ProductComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent,StudentComponent,ClassesComponent
+    AppComponent,StudentComponent,ClassesComponent,
+    HomeComponent,CategoryComponent, ProductComponent, LoginComponent, RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
