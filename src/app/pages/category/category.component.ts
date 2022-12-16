27f2cfdd76b619215aad965e753f4138a9091e96
billytrinh <quanghoa.trinh@gmail.com>
2022-12-16 11:38:03 +0700

    call api

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'page-category',
@@ -6,5 +7,23 @@ import {Component} from "@angular/core";
 })
 export class CategoryComponent{
   products: any = [];
+  limit: number = 10;
+  constructor(private http: HttpClient) {
+  }
 
+  ngOnInit(){
+      this.getProducts();
+  }
+
+  getProducts(){
+    const url = 'https://dummyjson.com/products?limit='+this.limit;
+    this.http.get<any>(url).subscribe(data=>{
+      this.products = data.products;
+    })
+  }
+
+  loadMore(){
+    this.limit+=10;
+    this.getProducts();
+  }
 }
