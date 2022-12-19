01a16411d1a36a3535c419325049c6e6c1921c0a
billytrinh <quanghoa.trinh@gmail.com>
2022-12-19 09:03:19 +0700

    services

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,5 +1,7 @@
 import {Component} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {Adapters} from "../../enums/adapters";
+import {ProductService} from "../../services/product.service";
 
 @Component({
   selector: 'page-category',
@@ -7,8 +9,8 @@ import {HttpClient} from "@angular/common/http";
 })
 export class CategoryComponent{
   products: any = [];
-  limit: number = 10;
-  constructor(private http: HttpClient) {
+  limit: number = Adapters.LIMIT;
+  constructor(private productService: ProductService) {
   }
 
   ngOnInit(){
@@ -16,9 +18,8 @@ export class CategoryComponent{
   }
 
   getProducts(){
-    const url = 'https://dummyjson.com/products?limit='+this.limit;
-    this.http.get<any>(url).subscribe(data=>{
-      this.products = data.products;
+    this.productService.getProducts(this.limit).subscribe(data=>{
+        this.products = data.products;
     })
   }
 
--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+import {Adapters} from "../../enums/adapters";
+import {ProductService} from "../../services/product.service";
 
 @Component({
   selector: 'app-product',
@@ -13,24 +15,23 @@ export class ProductComponent {
     id: 1,
     title: 'iPhone 9',
     price: 549,
-    thumbnail: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg'
+    thumbnail: Adapters.BASE_URL+ 'data/products/1/thumbnail.jpg'
   }
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) {
+  constructor(private route: ActivatedRoute,
+              private productService: ProductService) {
   }
-
   ngOnInit(){
     this.route.params.subscribe(params=>{
       this.id = params['id'];
       this.getProduct();
     })
   }
-
   /// call API:  https://dummyjson.com/products/ + this.id
   getProduct(){
     // call api
-    const url = 'https://dummyjson.com/products/'+this.id;
-    this.http.get<any>(url).subscribe(data=>{
+    this.productService.getProductDetail(this.id)
+      .subscribe(data=>{
       // set data to product
       this.product = data;
     })
